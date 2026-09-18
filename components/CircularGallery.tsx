c6bbe1f5f18c903e3ed56ca974b6400a90c6101f
Omshree <paridaomshree@gmail.com>
2026-09-18 13:45:15 +0530

    Update portfolio

--- components/CircularGallery.tsx
+++ components/CircularGallery.tsx
@@ -6,9 +6,9 @@ import { useEffect, useRef } from 'react';
 
 function debounce(func: Function, wait: number) {
   let timeout: ReturnType<typeof setTimeout>;
-  return function (...args: any[]) {
+  return (...args: any[]) => {
     clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
+    timeout = setTimeout(() => func(...args), wait);
   };
 }
 
